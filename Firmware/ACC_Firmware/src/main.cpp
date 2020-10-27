1ef3b0e436457834125a105008fead74abbef652
unknown <timbell478@gmail.com>
2020-10-27 12:29:04 -0400

    Operational wifiAP on ESP

--- Firmware/ACC_Firmware/src/main.cpp
+++ Firmware/ACC_Firmware/src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+
+const char* ssid = "MQP-Access-Point";
+const char* password = "test12345678";
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+
+String readAPOS() {
+  return "North";
+}
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("I am Alive");
+
+  // Setting the ESP as an access point
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  //Get Requests
+  server.on("/robotAPOS", HTTP_GET, [](AsyncWebServerRequest *request){  //Angular Position Get From Robot
+    request->send_P(200, "text/plain", readAPOS().c_str());
+  });
+
+  server.begin();
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+
 }
