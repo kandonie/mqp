871eca597f02d6569f158f0b1f8d9ccf1514d8d3
kandonie <kandonie@wpi.edu>
2020-10-27 12:38:50 -0400

    made a get request maybe

--- Hardware_Comms/jsonParser.py
+++ Hardware_Comms/jsonParser.py
@@ -1,3 +1,5 @@
+import requests
+
 class JSONParser:
     sensors = []
 
@@ -6,3 +8,10 @@ class JSONParser:
 
     def encodeJSON(self, motorData):
         pass
+
+
+# esp32 IP
+IP = "http://192.168.4.1"
+# sending get request and saving the response as response object
+r = requests.get(url = IP + "/readAPOS")
+print(r)
