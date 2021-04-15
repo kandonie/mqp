c452ef67f6b26e250adc8c50b7f0220d63b95c9c
Lauren Hatfield <lhatfield15@gmail.com>
2021-04-15 17:48:18 -0400

    quick fix

--- src/Guidance/States/PID_Tuner.py
+++ src/Guidance/States/PID_Tuner.py
@@ -13,6 +13,7 @@ class PIDTuner():
         self.wifi = wifi
         self.pidVals = {SetJSONVars.KP: PIDVals.KP_DEFAULT, SetJSONVars.KI: PIDVals.KI_DEFAULT,
                         SetJSONVars.KD: PIDVals.KD_DEFAULT, }
+        self.target = None
 
     def execute(self, robotData, stateArgs):
         """
@@ -25,13 +26,14 @@ class PIDTuner():
         gain_val = stateArgs[1]
         target = stateArgs[2]
         tuning_gain = "tuning_" + gain.value
-        if not self.hasSent or not self.pidVals[gain] == gain_val:
+        if not self.hasSent or not self.pidVals[gain] == gain_val or not self.target == target :
             self.pidVals[gain] = gain_val
             self.wifi.sendInfo({tuning_gain: 1,
                                 gain.value: gain_val,
                                 SetJSONVars.PID_TARGET.value: target})
             self.hasSent = True
             self.wifi.sendInfo({tuning_gain: 0})
+            self.target = target
             # return True
         return False
 
